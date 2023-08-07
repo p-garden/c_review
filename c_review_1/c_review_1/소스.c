5a807cef2e48857b72ebc2c69b804c90d4725fc1
Jeongwon Park <j2982477j@gmail.com>
2023-08-07 15:29:21 +0900

    1장 실습문제 4번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 int main() {
-	int n;
-	scanf("%d", &n);
-	printf("%d%d%d%d%d%d\n", n, n, n, n, n, n);
-	printf("%d    %d\n", n,n);
-	printf("%d    %d\n", n,n);
-	printf("%d%d%d%d%d%d\n", n, n, n, n, n, n);
+	int m, n;
+	m = 3, n = 5;
+printf("생일은 %d월 %d일입니다.", m, n);
 
 }
