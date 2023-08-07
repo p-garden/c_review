d881ad34ab6d91ae7d24d386ba39c524a9c1f5b6
Jeongwon Park <j2982477j@gmail.com>
2023-08-07 15:37:29 +0900

    1장 실습문제 6번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 int main() {
 	int m, n;
-	printf("생일 입력\n입력 월: ");
-	scanf("%d", &m);
-	printf("입력 일: ");
-	scanf("%d", &n);
+	printf("생일 입력\n입력 월 일: ");
+	scanf("%d %d", &m, &n);
 	printf("생일은 %d월 %d일입니다.", m, n);
 }
