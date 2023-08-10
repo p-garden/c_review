125faf8c1814a5359503e888fe03a289e35ebd3e
Jeongwon Park <j2982477j@gmail.com>
2023-08-10 16:36:06 +0900

    2장 실습문제 7-3번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main() {
-	int a;
-	printf("정수 입력(0~25) :");
-	scanf("%d", &a);
-	printf("%d번째 영어 대문자는 %c\n", a,'A'+a);
+	char a;
+	printf("소문자 입력: ");
+	scanf("%c", &a);
+	printf("대문자 출력: %c", a-'a' + 'A');
 
 
 }
