071aed970a31026e92d95f2aea63ff9bd5224fd8
Jeongwon Park <j2982477j@gmail.com>
2023-08-10 22:31:55 +0900

    3장 실습문제 3번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 int main() {
-	double n, m;
-	printf("가로: ");
-	scanf("%lf", &n);
-	printf("세로: ");
-	scanf("%lf", &m);
-	printf("넓이->%.2lf\n",n*m);
+	int a, b, c;
+	printf("가격 입력: ");
+	scanf("%d%d%d",&a,&b,&c);
+	printf("상품1:%8d원\n",a);
+	printf("상품2:%8d원\n", b);
+	printf("상품3:%8d원\n", c);
+
+
 }
