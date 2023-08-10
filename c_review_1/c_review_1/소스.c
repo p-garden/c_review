846f7c367697c26bc814227541f1eaa2be5b58d0
Jeongwon Park <j2982477j@gmail.com>
2023-08-10 22:28:24 +0900

    3장 실습문제 2-2

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 int main() {
-	int n;
-	printf("입력: ");
-	scanf("%x", &n);
-	printf("10진수    : %d\n", n);
-	printf("8진수    : %o\n", n);
+	double n, m;
+	printf("가로: ");
+	scanf("%lf", &n);
+	printf("세로: ");
+	scanf("%lf", &m);
+	printf("넓이->%.2lf\n",n*m);
 }
