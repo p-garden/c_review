56e4f71499060cce115211ad13e0b2e86296a886
Jeongwon Park <j2982477j@gmail.com>
2023-08-14 16:00:20 +0900

    4장 실습문제 2번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 int main() {
-	int n;
+	int n, h,m,s;
 	scanf("%d", &n);
-	printf("%dTTHO %dTHO %dHUN %dTEN %d", n / 10000, (n / 1000 %10), (n / 100 %10), (n / 10%10), n % 10);
+	h = n / 3600, n -= h * 3600;
+	m = n / 60, n -= m * 60;
+	s = n;
+	printf("%d:%d:%d", h, m, s);
+
+	
 
 
 
