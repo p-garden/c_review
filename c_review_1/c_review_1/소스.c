19a0d918baed67ce786a64a48687d389846f2d76
Jeongwon Park <j2982477j@gmail.com>
2023-08-14 17:22:55 +0900

    과제1 4번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main() {
-	int m;
+	int m,r,d;
 	scanf("%d", &m);
-	(m / 100 == m / 10 % 10 && m / 10 % 10 == m % 10) ? printf("3") : (m / 100 != m / 10 % 10 && m / 10 % 10 != m % 10 && m / 100 != m % 10) ? printf("1") : printf("2");
-	
-
+	r = m % 10 * 1000 + m / 10 % 10 * 100 + m / 100 % 10 * 10 + m / 1000;
+	d = m > r ? m - r : r - m;
+	printf("%d", d);
 	
 
 
