5682080dc9fcffe680410222174a8d52a525ba7d
Jeongwon Park <j2982477j@gmail.com>
2023-08-14 16:20:13 +0900

    4장 실습문제 9번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 int main() {
-	char m;
-	int n;
-	scanf("%d", &n);
-	n % 2 == 0 ? printf("even") :printf("odd");
+	int n,m;
+	scanf("%d%d", &n, &m);
+	m > n ? printf("%d", m) : printf("%d", n);
 
 	
 
