218c34517ffac2ce6dda9215d69acab23c0d66ef
Jeongwon Park <j2982477j@gmail.com>
2023-08-15 17:32:08 +0900

    5장 실습문제 9번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 int main() {
-	int y;
-	scanf("%d", &y);
-
-	if (y % 4 == 0) {
-		if (y % 100 == 0)
-			if (y % 400 == 0)
-				printf("leap year");
+	int y1,m1,d1,y2,m2,d2;
+	scanf("%d/%d/%d", &y1, &m1, &d1);
+	scanf("%d/%d/%d", &y2, &m2, &d2);
+	if (y1 < y2)
+		printf("%d/%d/%d", y1, m1, d1);
+	else if (y2 < y1)
+		printf("%d/%d/%d", y2, m2, d2);
+	else {
+		if (m1 < m2)
+			printf("%d/%d/%d", y1, m1, d1);
+		else if (m2 < m1)
+			printf("%d/%d/%d", y2, m2, d2);
+		else {
+			if (d1 < d2)
+				printf("%d/%d/%d", y1, m1, d1);
+			else if (d2 < d1)
+				printf("%d/%d/%d", y2, m2, d2);
 			else
-				printf("common year");
-		else
-			printf("leap year");
+				printf("%d/%d/%d", y1, m1, d1);
+		}
 	}
-	else
-		printf("common year");
-
-
-	
-
-	
 	
 
 
