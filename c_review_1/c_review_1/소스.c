46d1d7effdf476cc8f3c0169b8975de621c00cb6
Jeongwon Park <j2982477j@gmail.com>
2023-08-15 16:26:08 +0900

    5장 실습문제 2번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 int main() {
-	int a;
-	scanf("%d", &a);
-	if (a < 0)
-		printf("cold, indoor");
-	else if (a < 40)
-		printf("moderate, outdoor");
+	int a,b;
+	scanf("%d%d", &a, &b);
+	if (a*a == b)
+		printf("%d*%d=%d", a, a, b);
+	else if (b*b == a)
+		printf("%d*%d=%d", b, b, a);
 	else
-		printf("hot, indoor");
-
+		printf("none");
 }
