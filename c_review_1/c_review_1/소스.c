9cb564b447ae805aa2768bd0135c7713873ae5ff
Jeongwon Park <j2982477j@gmail.com>
2023-08-15 16:38:12 +0900

    5장 실습문제 3번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 int main() {
-	int a,b;
-	scanf("%d%d", &a, &b);
-	if (a*a == b)
-		printf("%d*%d=%d", a, a, b);
-	else if (b*b == a)
-		printf("%d*%d=%d", b, b, a);
-	else
-		printf("none");
+	int a, b, c, d, e,sum=0;
+	scanf("%d%d%d%d%d", &a, &b, &c, &d, &e);
+	if (a > 0)
+		sum += a;
+	if (b > 0)
+		sum += b;
+	if (c > 0)
+		sum += c;
+	if (d > 0)
+		sum += d;
+	if (e > 0)
+		sum += e;
+	printf("%d", sum);
+
+
+
 }
