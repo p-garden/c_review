6b31427f7f16621aa928a1ae8fcce16819827e43
Jeongwon Park <j2982477j@gmail.com>
2023-08-15 21:15:29 +0900

    과제2 1번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,26 +1,13 @@
 #include <stdio.h>
 int main() {
-	int a1,a2,a3,g1,g2,g3,s=0,b=0;
-	scanf("%d%d%d", &a1, &a2, &a3);
-	scanf("%d%d%d", &g1, &g2, &g3);
-	if (a1 == g1)
-		s++;
-	if (a2 == g2)
-		s++;
-	if (a3 == g3)
-		s++;
-	if (a1 == g2)
-		b++;
-	if (a1 == g3)
-		b++;
-	if (a2 == g1)
-		b++;
-	if (a2 == g3)
-		b++;
-	if (a3 == g1)
-		b++;
-	if (a3 == g2)
-		b++;
-
-	printf("%dS%dB", s, b);
+	char a;
+	scanf("%c", &a);
+	if ('a' <= a && a <= 'z')
+		printf("%c", a - ('a' - 'A'));
+	else if ('A' <= a && a <= 'Z')
+		printf("%c", a + ('a' - 'A'));
+	else
+		printf("%c", a);
+	
+	
 }
