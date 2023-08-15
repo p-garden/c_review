ea2d0a3c2237d669a4bfb8030cb7743151f4dcce
Jeongwon Park <j2982477j@gmail.com>
2023-08-15 21:38:29 +0900

    과제2 3번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,75 +1,17 @@
 #include <stdio.h>
 int main() {
-	int a, b, c,sum=0;
+	int a, b, c;
 	scanf("%d%d%d", &a, &b, &c);
-	if (a == 1) {
-		printf("Pizza\n");
-		sum += 15000;
-	}
-	else if (a == 2) {
-		printf("Burger\n");
-		sum += 5000;
-	}
-	else if (a == 3) {
-		printf("Salad\n");
-		sum += 4500;
-	}
-	else if (a == 4) {
-		printf("Ice cream");
-		sum += 800;
-	}
-	else if (a == 5) {
-		printf("Beverage\n");
-		sum += 500;
-	}
-	else
-		printf("None\n");
+	
+	if (a + b == c)
+		printf("+\n");
+	if (a-b == c)
+		printf("-\n");
+	if (a * b == c)
+		printf("*\n");
+	if (a / b == c)
+		printf("/\n");
+	if (a + b != c && a - b != c && a*b != c && a / b != c)
+		printf("Incorrect");
 
-	if (b == 1) {
-		printf("Pizza\n");
-		sum += 15000;
-	}
-	else if (b == 2) {
-		printf("Burger\n");
-		sum += 5000;
-	}
-	else if (b == 3) {
-		printf("Salad\n");
-		sum += 4500;
-	}
-	else if (b == 4) {
-		printf("Ice cream");
-		sum += 800;
-	}
-	else if (b == 5) {
-		printf("Beverage\n");
-		sum += 500;
-	}
-	else
-		printf("None\n");
-
-	if (c == 1) {
-		printf("Pizza\n");
-		sum += 15000;
-	}
-	else if (c == 2) {
-		printf("Burger\n");
-		sum += 5000;
-	}
-	else if (c == 3) {
-		printf("Salad\n");
-		sum += 4500;
-	}
-	else if (c == 4) {
-		printf("Ice cream");
-		sum += 800;
-	}
-	else if (c == 5) {
-		printf("Beverage\n");
-		sum += 500;
-	}
-	else
-		printf("None\n");
-
-	printf("Total:%dwon", sum);
 }
