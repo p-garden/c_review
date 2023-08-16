6344911453d2731a46ea8117a3260ec204978bbd
Jeongwon Park <j2982477j@gmail.com>
2023-08-16 16:01:40 +0900

    6장 실습문제 5-1

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 int main() {
-	int a,b,tmp,i,gcd;
-	scanf("%d%d", &a, &b);
-	if (a > b) {
-		tmp = a;
-		a = b; 
-		b = tmp;
-	}
-	for (i = 1; i <= a; i++) {
-		if (a % i == 0 && b % i == 0)
-			gcd = i;
+	int a,i,j;
+	scanf("%d", &a);
+	for (i = 0; i < a; i++) {
+		for (j = 0; j <= i; j++)
+			printf("*");
+		printf("\n");
 	}
 	
-	printf("%d", gcd);
 }
