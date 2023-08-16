e39be13b2581fe64c312c0e9dcc8ff873e20e159
Jeongwon Park <j2982477j@gmail.com>
2023-08-16 16:06:50 +0900

    6장 실습문제 5-4

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -2,8 +2,8 @@
 int main() {
 	int a,i,j;
 	scanf("%d", &a);
-	for (i = 0; i < a; i++) {
-		for (j = 0; j <(2*i+1) ; j++)
+	for (i = a; i > 0; i--) {
+		for (j = 0; j < (i*2-1) ; j++)
 			printf("*");
 		printf("\n");
 	}
