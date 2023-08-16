2d8d4e0ffa5b9f16198ac5e830db19875325002c
Jeongwon Park <j2982477j@gmail.com>
2023-08-16 15:32:42 +0900

    6장 실습문제 1번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,26 +1,10 @@
 #include <stdio.h>
 int main() {
-	int a, b, c, d, e,max,min;
-	scanf("%d%d%d%d%d",&a, &b, &c, &d, &e);
-	max = a, min = a;
-	if (max < b)
-		max = b;
-	if (max < c)
-		max = c;
-	if (max < d)
-		max = d;
-	if (max < e)
-		max = e;
-
-	if (min > b)
-		min = b;
-	if (min > c)
-		min = c;
-	if (min > d)
-		min = d;
-	if (min > e)
-		min = e;
-
-	printf("max: %d\nmin: %d\n", max, min);
+	int a,i;
+	scanf("%d", &a);
 	
+	for (i = 1; i <= a; i++) {
+		if (a % i == 0)
+			printf("%d ", i);
+	}
 }
