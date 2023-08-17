13712cec0a1df5bb1af8fb778323546cc9f7e605
Jeongwon Park <j2982477j@gmail.com>
2023-08-17 15:40:34 +0900

    1차 모의인증 4번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 int main() {
-	int cnt_a=0,cnt_A=0, cnt_n=0,cnt_etc=0;
-	char a;
-	scanf("%c", &a);
-	while (a != '*') {
-		if ('a' <= a && a <= 'z')
-			cnt_a++;
-		else if ('A' <= a && a <= 'Z')
-			cnt_A++;
-		else if ('0' <= a && a <= '9')
-			cnt_n += (a-'0');
-		else
-			cnt_etc++;
-		scanf("%c", &a);
+	int m1,m2,n1,n2,i,j;
+	scanf("%d%d", &m1, &m2);
+	scanf("%d%d", &n1, &n2);
+	for (i = n1; i <= n2; i++) {
+		for (j = m1; j <= m2; j++) {
+			printf(" %d", i*j);
+		}
+		printf("\n");
 	}
-	printf("%d %d %d %d", cnt_A, cnt_a, cnt_n, cnt_etc);
 }
