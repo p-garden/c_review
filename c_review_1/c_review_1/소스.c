2a24041033224d26ea15e7c20b8b96e25e0227d8
Jeongwon Park <j2982477j@gmail.com>
2023-08-17 15:47:43 +0900

    1차 모의인증 5번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main() {
-	int m1,m2,n1,n2,i,j;
-	scanf("%d%d", &m1, &m2);
-	scanf("%d%d", &n1, &n2);
-	for (i = n1; i <= n2; i++) {
-		for (j = m1; j <= m2; j++) {
-			printf(" %d", i*j);
-		}
-		printf("\n");
+	int a,tmp1,tmp2;
+	scanf("%d", &a);
+	tmp1 = a;
+	while (tmp1) {
+		tmp2 = tmp1 % 10;
+		a *= 10;
+		a += tmp2;
+		tmp1 /= 10;
 	}
+	printf("%d\n%d", a,a%131);
 }
