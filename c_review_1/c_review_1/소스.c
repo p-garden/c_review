22288aa91215336fbd37014d455f5fe0cffd3e3b
Jeongwon Park <j2982477j@gmail.com>
2023-08-17 15:30:35 +0900

    1차 모의인증 2번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 int main() {
-	int a,b;
-	float avg;
-	scanf("%d%d", &a, &b);
-	avg = (a *0.4+ b*0.6);
-	if (avg >= 85.5)
-		printf("A ");
-	else if (avg >= 75.5)
-		printf("B ");
-	else if (avg >= 60.0)
-		printf("C ");
-	else
-		printf("F ");
-
-	if (avg >= 60 || b >= 90)
-		printf("PASS");
-	else
-		printf("FAIL");
+	int n,i,pri,a,b;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++) {
+		scanf("%d%d%d", &pri, &a, &b);
+		if (pri - a < 0 || pri - b < 0) {
+			if (pri - a < 0 && pri - b < 0)
+				printf("draw\n");
+			else if (pri - a < 0)
+				printf("B\n");
+			else
+				printf("A\n");
+		}
+		else if (pri - a < pri - b)
+			printf("A\n");
+		else if (pri - a > pri - b)
+			printf("B\n");
+		else
+			printf("draw\n");
+	}
 }
