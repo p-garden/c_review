64029db93f1d77e83afd2b05c3ef03ef55fc0f48
Jeongwon Park <j2982477j@gmail.com>
2023-08-17 16:57:11 +0900

    1차 인증 1번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 int main() {
-	int a,tmp1,tmp2;
-	scanf("%d", &a);
-	tmp1 = a;
-	while (tmp1) {
-		tmp2 = tmp1 % 10;
-		a *= 10;
-		a += tmp2;
-		tmp1 /= 10;
-	}
-	printf("%d\n%d", a,a%131);
+	int kor1, eng1, kor2, eng2;
+	float avg1, avg2,tmp;
+	scanf("%d%d%d%d", &kor1, &eng1, &kor2, &eng2);
+	avg1 = (kor1 + eng1) / 2.0, avg2 = (kor2 + eng2) / 2.0;
+	if (avg1 > avg2)
+		tmp = avg1;
+	else
+		tmp = avg2;
+
+	if (kor1 < 60 || eng1 < 60 || kor2 < 60 || eng2 < 60)
+		printf("F");
+	else if (tmp >= 90)
+		printf("A+");
+	else if (tmp >= 80)
+		printf("A");
+	else if (tmp >= 70)
+		printf("B+");
+	else if (tmp >= 60)
+		printf("B");
 }
