604266d8607389b5a3535dbb82487750f812a92b
Jeongwon Park <j2982477j@gmail.com>
2023-08-18 15:37:24 +0900

    7장 실습문제 1번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,28 +1,16 @@
 #include <stdio.h>
 int main() {
-	int n,i,j,cnt1,cnt2;
-	scanf("%d", &n);
-	while (n > 0) {
-		cnt1 = 0;
-		for (i = 1; i <= n; i++) {
-			if (n%i == 0)
-				cnt1++;
-		}
-		if (cnt1 == 2) {
-			printf("%d", n);
-			for (i = n + 1; ; i++) {
-				cnt2 = 0;
-				for (j = 1; j <= i; j++) {
-					if (i%j == 0)
-						cnt2++;
-				}
-				if (cnt2 == 2) {
-					printf(" %d\n", i);
-						break;
-				}
-			}
-		}
-		scanf("%d", &n);
+	int n[5],i,sum=0;
+	float  avg;
+	
+	for (i = 0; i < 5; i++) {
+		scanf("%d", &n[i]);
+		sum += n[i];
 	}
+	avg = sum / 5.0;
 
+	for (i = 0; i < 5; i++)
+		if (n[i] > avg)
+			printf("%d\n", n[i]);
+	
 }
