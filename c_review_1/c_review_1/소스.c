c496575ffbc5e0698bf9d0b0d8dc81524b93fd5a
Jeongwon Park <j2982477j@gmail.com>
2023-08-18 18:30:43 +0900

    과제4 1번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,41 +1,60 @@
 #include <stdio.h>
 int main() {
-	int i,j, x[3][3],y[2][2];
-
-	for (i = 0; i <3; i++) 
-		for (j=0;j<3;j++)
-			scanf("%d", &x[i][j]);
-	for (i = 0; i < 2; i++)
-		for (j = 0; j < 2; j++)
-			scanf("%d", &y[i][j]);
-
-	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 3; j++) {
-			printf(" %d", x[i][j]);
-		}
-		printf("\n");
+	int i,j,n,sco[100],mat[100],pos,ins,flg=0;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+		scanf("%d", &sco[i]);
+	mat[0] = 0;
+	for (i = 1; i < n; i++) {
+		if (i == 1 || i == 2 || i == 4 || i == 8 || i == 16 || i == 32 || i == 64)
+			mat[i] = mat[i - 1] + 1;
+		else
+			mat[i] = mat[i - 1];
 	}
-	for (i = 0; i < 2; i++) {
-		for (j = 0; j < 2; j++) {
-			printf(" %d", y[i][j]);
+	pos = n;
+	for (i = 0; i < n; i++) {
+		if (sco[i] < 0 || sco[i] > 100) {
+			flg = 1;
+			printf("ERROR");
 		}
-		printf("\n");
 	}
-
-	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 3; j++) {
-			if (x[i][j] == y[0][0]) {
-				if (x[i][j + 1] == y[0][1] && x[i + 1][j] == y[1][0] && x[i + 1][j + 1] == y[1][1])
-					printf("%d %d", i, j);
-				break;
+	while (mat[n-1]&&flg==0) {
+		ins = 0;
+		for (i = 0; i < pos; i+=2) {
+			if (pos % 2 == 1) {
+				if (i == pos - 1) {
+					sco[ins] = sco[i];
+					printf("%d ", sco[ins]);
+					ins++;
+				}
+				else if (sco[i] < sco[i + 1]) {
+					sco[ins] = sco[i + 1];
+					printf("%d ", sco[ins]);
+					ins++;
+				}
+				else {
+					sco[ins] = sco[i];
+					printf("%d ", sco[ins]);
+					ins++;
+				}
 			}
-			if (i == 2 && j == 2)
-				printf("none");
+			else {
+				if (sco[i] < sco[i + 1]) {
+					sco[ins] = sco[i + 1];
+					printf("%d ", sco[ins]);
+					ins++;
+				}
+				else {
+					sco[ins] = sco[i];
+					printf("%d ", sco[ins]);
+					ins++;
+				}
+			}
 		}
+		printf("\n");
+		pos = ins;
+		mat[n-1]--;
 	}
-
-
-
 	
 
 }
