68e137fa7fac04267203d62a0aa4ff550d15477a
Jeongwon Park <j2982477j@gmail.com>
2023-08-18 16:27:31 +0900

    7장 실습문제 7번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 int main() {
-	int i,j;
-	char  a[5];
+	int i,j,flg=0,cnt, a[10];
 
-	for (i = 0; i < 5; i++) {
-		scanf("%c", &a[i]);
+	for (i = 0; i <10; i++) 
+		scanf("%d", &a[i]);
+	
+	for (i = 0; i < 10; i++) {
+		for (j = i-1; j >= 0; j--) {
+			if (a[i] == a[j])
+				flg=1;
+		}
+		if (flg == 1) {
+			flg = 0;
+			continue;
+		}
+		cnt = 0;
+		for (j = 0; j < 10; j++) {
+			if (a[i] == a[j])
+				cnt++;
+		}
+		printf("%d %d\n", a[i], cnt);
 	}
 	
-	for (i= 5; i >0; i--) {
-		for (j = 0; j < 5; j++) {
-			printf("%c", a[(i +j)%5]);
-		}
-		printf("\n");
-	}
+	
 }
