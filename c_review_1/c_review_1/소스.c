9dfcbc6134b12d48455431ba80881377f13ab179
Jeongwon Park <j2982477j@gmail.com>
2023-08-18 16:43:40 +0900

    7장 실습문제 11번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
 int main() {
-	int i,j,tmp, a[5];
+	int i,j, a[3][2],sum;
 
-	for (i = 0; i <5; i++) 
-		scanf("%d", &a[i]);
+	for (i = 0; i <3; i++) 
+		for (j=0;j<2;j++)
+			scanf("%d", &a[i][j]);
 
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 4-i; j++) {
-			if (a[j] >= a[j+1]) {
-				tmp = a[j];
-				a[j] = a[j+1];
-				a[j+1] = tmp;
-			}
+	for (i = 0; i < 3; i++) {
+		sum = 0;
+		for (j = 0; j < 2; j++) {
+			sum += a[i][j];
 		}
+		printf("%d\n", sum);
 	}
-	for (i = 0; i < 5; i++)
-		printf("%d\n", a[i]);
 
-	
-	
-	
-	
 }
