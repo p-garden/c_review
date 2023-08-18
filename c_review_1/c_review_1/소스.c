c62d1d85e6821a4256e93540f7e86aec79defc4d
Jeongwon Park <j2982477j@gmail.com>
2023-08-18 15:46:59 +0900

    7장 실습문제 2번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 int main() {
-	int n[5],i,sum=0;
-	float  avg;
-	
-	for (i = 0; i < 5; i++) {
-		scanf("%d", &n[i]);
-		sum += n[i];
-	}
-	avg = sum / 5.0;
+	int n[5],i,sum=0,flg=0,cnt=0;
+	char cat[7];
 
-	for (i = 0; i < 5; i++)
-		if (n[i] > avg)
-			printf("%d\n", n[i]);
+	for (i = 0; i < 7; i++) {
+		scanf("%c", &cat[i]);
+	}
 	
+	for (i = 0; i < 7; i++) {
+		if (cat[i] != 'c' && cat[i] != 'a'&&cat[i] != 't') {
+			flg = 0;
+			continue;
+		}
+		else {
+			if (cat[i] == 'c' && flg == 0)
+				flg++;
+			else if (cat[i] == 'a' &&flg == 1)
+				flg++;
+			else if (cat[i] == 't'&&flg == 2)
+				flg++;
+			else
+				flg = 0;
+		}
+		if (flg == 3) {
+			cnt++;
+			flg = 0;
+		}
+	}
+	printf("%d", cnt);
 }
