ba8178d6542b424ad961dbe87276b721de4be54c
Jeongwon Park <j2982477j@gmail.com>
2023-08-19 21:39:55 +0900

    과제4 문제 4-2

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 int main() {
-	int a[20], i, f, r, m, tmp;
-	scanf("%d%d%d", &f, &r, &m);
-	for (i = 0; i < 20; i++) 
-		a[i] = i + 1;
-	
-	tmp = a[r];
-	for (i = r ; i >r-m ; i--) {
-		a[i] = a[i - 1];
+	int a[20], b[20], i, j, n, max[10], min[10], max_tmp, min_tmp, k;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+		scanf("%d", &a[i]);
+
+	for (i = 0; i < n; i++) {
+		b[i] = a[n - i - 1];
+		printf(" %d", b[i]);
 	}
-	a[r - m+1] = tmp;
-	
-	for (i = 0; i < 20; i++)
-		printf("%d ", a[i]);
+	printf("\n");
+	k = 0;
+	for (i = 0; i < n; i += 3) {
+		max_tmp = a[i], min_tmp = a[i];
+		for (j = i; j < i + 3; j++) {
+			if (j >= n)
+				break;
+			if (max_tmp < a[j])
+				max_tmp = a[j];
+			if (min_tmp > a[j])
+				min_tmp = a[j];
+		}
+		max[k] = max_tmp, min[k] = min_tmp;
+		k++;
+	}
+	for (i = 0; i < k; i++)
+		printf(" %d", max[i]);
+	printf("\n");
+	for (i = 0; i < k; i++)
+		printf(" %d", min[i]);
 }
