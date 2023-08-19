5f62560d0c3c364fdab2909948b5f5a2cfca64c8
Jeongwon Park <j2982477j@gmail.com>
2023-08-19 22:07:42 +0900

    과제4 4-3번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-	int a[20], b[20], i, j, n, max[10], min[10], max_tmp, min_tmp, k;
+	int a[20], b[20], i, j, n, max[10], min[10], max_tmp, min_tmp, k,l,h;
 	scanf("%d", &n);
 	for (i = 0; i < n; i++)
 		scanf("%d", &a[i]);
@@ -29,4 +29,31 @@ int main() {
 	printf("\n");
 	for (i = 0; i < k; i++)
 		printf(" %d", min[i]);
+	printf("\n");
+
+	l = k;
+	while (l >1 ) {
+		k = l;
+		l = 0;
+		for (i = 0; i < k; i += 3) {
+			max_tmp = max[i], min_tmp = min[i];
+			for (j = i; j < i + 3; j++) {
+				if (j >= k)
+					break;
+				if (max_tmp < max[j])
+					max_tmp = max[j];
+				if (min_tmp > min[j])
+					min_tmp = min[j];
+			}
+			max[l] = max_tmp, min[l] = min_tmp;
+			l++;
+		}
+
+		for (i = 0; i < l; i++)
+			printf(" %d", max[i]);
+		printf("\n");
+		for (i = 0; i < l; i++)
+			printf(" %d", min[i]);
+		printf("\n");
+	}
 }
