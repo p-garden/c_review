2256aac55e9ac6b0bea281a147d2d2182e2e1f90
Jeongwon Park <j2982477j@gmail.com>
2023-08-23 16:01:55 +0900

    8장 실습문제 14번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,37 +1,32 @@
 #include <stdio.h>
-int a1, a2, a3;
-int count_strike(int, int, int);
-int count_ball(int, int, int);
+int is_prime(int);
+int next_prime(int);
 int main() {
-	int b1,b2,b3,flg=0,cnt_s,cnt_b;
-	scanf("%d%d%d", &a1,&a2,&a3);
-	while (flg == 0) {
-		scanf("%d%d%d", &b1, &b2, &b3);
-		cnt_s = count_strike(b1, b2, b3);
-		cnt_b = count_ball(b1, b2, b3);
-		printf("%dS%dB\n", cnt_s, cnt_b);
-		if (cnt_s == 3)
-			flg = 1;
-
+	int n, m,cnt=0,nxt;
+	scanf("%d %d", &n, &m);
+	while (cnt<m){
+		nxt = next_prime(n);
+		printf(" %d", nxt);
+		cnt++;
+		n = nxt;
 	}
 }
-int count_strike(a, b, c) {
-	int cnt = 0;
-	if (a == a1)
-		cnt++;
-	if (b == a2)
-		cnt++;
-	if (c == a3)
-		cnt++;
-	return cnt;
+int is_prime(x) {
+	int i,cnt=0;
+	for (i = 1; i <= x; i++) {
+		if (x%i == 0)
+			cnt++;
+	}
+	if (cnt == 2)
+		return 1;
+	else
+		return 0;
 }
-int count_ball(a, b, c) {
-	int cnt = 0;
-	if (a == a2 || a == a3)
-		cnt++;
-	if ( b == a1 || b == a3)
-		cnt++;
-	if (c == a1 || c == a2 )
-		cnt++;
-	return cnt;
+int next_prime(x) {
+	int i;
+	for (i = x + 1; ; i++) {
+		if (is_prime(i) == 1) {
+			return i;
+		}
+	}
 }
