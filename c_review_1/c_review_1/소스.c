2f73cf5340be5f40897ac836c136e513339543af
Jeongwon Park <j2982477j@gmail.com>
2023-08-23 15:47:07 +0900

    8장 실습문제  11번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
-int n=0;
-void die1(int);
-void die2(int,int);
-void die3(int,int,int);
+int a1, a2, a3;
+int count_strike(int, int, int);
+int count_ball(int, int, int);
 int main() {
-	int i;
-	scanf("%d", &n);
-	for (i = 1; i <= 6; i++) {
-		die1(i);
-	}
-}
-void die1(x) {
-	int i;
-	for (i = 1; i <= 6; i++) {
-		die2(x, i);
-	}
+	int b1,b2,b3,flg=0,cnt_s,cnt_b;
+	scanf("%d%d%d", &a1,&a2,&a3);
+	while (flg == 0) {
+		scanf("%d%d%d", &b1, &b2, &b3);
+		cnt_s = count_strike(b1, b2, b3);
+		cnt_b = count_ball(b1, b2, b3);
+		printf("%dS%dB\n", cnt_s, cnt_b);
+		if (cnt_s == 3)
+			flg = 1;
 
-}
-void die2(x, y) {
-	int i;
-	for (i=1;i<=6;i++) {
-		die3(x, y, i);
 	}
 }
-void die3(x, y, z) {
-	if (x + y + z == n)
-		printf("%d %d %d\n", x, y, z);
-
+int count_strike(a, b, c) {
+	int cnt = 0;
+	if (a == a1)
+		cnt++;
+	if (b == a2)
+		cnt++;
+	if (c == a3)
+		cnt++;
+	return cnt;
+}
+int count_ball(a, b, c) {
+	int cnt = 0;
+	if (a == a2 || a == a3)
+		cnt++;
+	if ( b == a1 || b == a3)
+		cnt++;
+	if (c == a1 || c == a2 )
+		cnt++;
+	return cnt;
 }
