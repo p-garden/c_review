affad50b002f0b70418eb17b8aab5ef7de7880fd
Jeongwon Park <j2982477j@gmail.com>
2023-08-24 19:35:35 +0900

    과제5 2-2번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,32 +1,61 @@
 #include <stdio.h>
-int is_prime(int);
-int next_prime(int);
+char decrypt(char, int);
+char encrypt(char, int);
 int main() {
-	int n, m,cnt=0,nxt;
-	scanf("%d %d", &n, &m);
-	while (cnt<m){
-		nxt = next_prime(n);
-		printf(" %d", nxt);
-		cnt++;
-		n = nxt;
+	int n,m,len=0,i;
+	char ch,ch_li[101],tmp, en_li[101];
+	scanf("%c", &ch);
+	while (ch != '*') {
+		ch_li[len] = ch;
+		len++;
+		scanf("%c", &ch);
 	}
+	scanf("%d%d", &n, &m);
+	for (i = 0; i < len; i++) {
+		tmp=decrypt(ch_li[i],n);
+		en_li[i] = tmp;
+		printf("%c", tmp);
+	}
+	printf("\n");
+	for (i = 0; i < len; i++) {
+		tmp = encrypt(en_li[i], m);
+		printf("%c",tmp);
+	}
+	return 0;
 }
-int is_prime(x) {
-	int i,cnt=0;
-	for (i = 1; i <= x; i++) {
-		if (x%i == 0)
-			cnt++;
+char decrypt(char ch, int m) {
+	char ch1;
+	if ('a' <= ch && ch <= 'z') {
+		ch1 = ch - 'a' - m;
+		if ('a' <= (ch1 + 'a') && (ch1 + 'a') <= 'z')
+			return ch1 + 'a';
+		else  if (ch1 < 0 && ('a' <= (ch1 + 'z' + 1) && (ch1 + 'z' + 1) <= 'z'))
+			return (ch1 + 'z' + 1);
 	}
-	if (cnt == 2)
-		return 1;
+	else if ('A' <= ch && ch <= 'Z') {
+		ch1 = ch - 'A' - m;
+		if ('A' <= (ch1 + 'A') && (ch1 + 'A') <= 'Z')
+			return ch1 + 'A';
+		else  if (ch1 < 0 && ('A' <= (ch1 + 'Z' + 1) && (ch1 + 'Z' + 1) <= 'Z'))
+			return (ch1 + 'Z' + 1);
+	}
 	else
-		return 0;
+		return ch;
 }
-int next_prime(x) {
-	int i;
-	for (i = x + 1; ; i++) {
-		if (is_prime(i) == 1) {
-			return i;
-		}
+char encrypt(char ch, int m) {
+	char ch1;
+	if ('a' <= ch && ch <= 'z') {
+		if ('a' <= (ch+m) && (ch + m) <= 'z')
+			return(ch + m);
+		else if ((ch + m) >'z')
+			return (((ch + m) - 'a') % 26) + 'a';
 	}
+	else if ('A' <= ch && ch <= 'Z') {
+		if ('A' <= (ch + m) && (ch + m) <= 'Z')
+			return (ch + m);
+		else if ((ch + m) >'Z')
+			return (((ch + m) - 'A') % 26) + 'A';
+	}
+	else 
+		return ch; 
 }
