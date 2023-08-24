95a1925e9e75ddd2708ac0902e9d9759fcfa1fa1
Jeongwon Park <j2982477j@gmail.com>
2023-08-24 19:51:32 +0900

    과제5 3번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,56 +1,42 @@
 #include <stdio.h>
-int num_digit(int);
-int convert_top(int, int);
-int convert_bottom(int);
+int multiple(int, int);
+int maximum(int, int);
+int digit_maximum(int);
 int main() {
-	int n, i, n_cnt;
-	scanf("%d", &n);
-	n_cnt = num_digit(n);
-	if (n_cnt % 2 == 1) {
-		while (n) {
-			n = convert_top(n, n_cnt);
-			n_cnt -= 2;
+	int n,m,k,i,max=0;
+	scanf("%d%d%d", &n, &m, &k);
+	for (i = n; i <= m; i++) {
+		if (multiple(i,k) == 1) {
+			if (max < digit_maximum(i))
+				max = digit_maximum(i);
+		}
+		else {
+			if (max < (i%k))
+				max = i % k;
 		}
 	}
-	else {
-		while (n)
-			n = convert_bottom(n);
-	}
+	printf("%d", max);
 	return 0;
 }
-int num_digit(n) {
-	int dig = 0;
-	while (n) {
-		n /= 10;
-		dig++;
-	}
-	return dig;
-}
-int convert_top(n, dig) {
-	int n_10 = 1, i;
-	for (i = 1; i < dig; i++)
-		n_10 *= 10;
-	n_10 /= 10;
-	if (dig == 1)
-		printf("*");
-	else {
-		if (('a' <= (n / n_10) && (n / n_10) <= 'z') || ('A' <= (n / n_10) && (n / n_10) <= 'Z'))
-			printf("%c", n / n_10);
-		else
-			printf("*");
-	}
-	if (dig == 1)
+int multiple(int x, int y) {
+	if (x%y == 0)
+		return 1;
+	else
 		return 0;
-	else
-		return n % n_10;
 }
-int convert_bottom(n) {
-	if (('a' <= (n % 100) && (n % 100) <= 'z') || ('A' <= (n % 100) && (n % 100) <= 'Z'))
-		printf("%c", (n % 100));
+int maximum(int x, int y) {
+	if (x > y)
+		return x;
 	else
-		printf("*");
-
-
-	return (n / 100);
-
+		return y;
+}
+int digit_maximum(int x) {
+	int i,cur,max;
+	max = x % 10;
+	while (x) {
+		cur = x % 10;
+		x /= 10;
+		max = maximum(max, cur);
+	}
+	return max;
 }
