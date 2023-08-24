7416d0774cbe83b6f53e0e456d60d0c05c2638e1
Jeongwon Park <j2982477j@gmail.com>
2023-08-24 20:49:35 +0900

    과제5 5-3번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
-int count(int, int);
+int add_digits(int);
 int main() {
-	int n, m, k, i, cnt[10] = {0};
+	int n, cur_sum, min, min_con, tmp;
 	scanf("%d", &n);
-	while (n > 0) {
-		for (i = 0; i <= 9; i++) {
-			cnt[i] += count(n, i);
+	tmp = add_digits(n);
+	while (tmp / 10) 
+		tmp = add_digits(tmp);
+	min = tmp;
+	min_con = n;
+	while (n >= 0) {
+		cur_sum = add_digits(n);
+			while (cur_sum / 10)
+			cur_sum = add_digits(cur_sum);
+		if (cur_sum < min) {
+			min = cur_sum;
+			min_con = n;
 		}
 		scanf("%d", &n);
 	}
-	for (i = 0; i < 10; i++)
-		printf("%d ", cnt[i]);
+	printf("%d %d", min_con, min);
 	return 0;
 }
-int count(int n, int m) {
-	int cnt = 0;
+
+int add_digits (int n) {
+	int sum = 0;
 	while (n) {
-		if (n % 10 == m)
-			cnt++;
+		sum += n % 10;
 		n /= 10;
 	}
-	return cnt;
+	return sum;
 }
