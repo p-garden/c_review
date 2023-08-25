cc39dd0f6d7b5de6d4d5fdf9c2c21adee3e30da2
Jeongwon Park <j2982477j@gmail.com>
2023-08-25 14:35:54 +0900

    9장 실습문제 4번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 int main() {
-	int ar[50] = { 0 }, n,i,cnt=0;
-	int *p;
+	int x[3], n,i,cnt=0,*tmp;
+	int *px,*py;
 
-	scanf("%d", &n);
-	for (p = ar; p < ar + n;p++) {
-		scanf("%d", p);
-		if (*p == 0)
-			break;
-		cnt++;
+	for (px = x; px < x + 3; px++)
+		scanf("%d", px);
+	
+	for (px = x+2; px >=x; px--) {
+		for (py = x; py < px; py++) {
+			if (*py < *(py + 1)) {
+				tmp = *py;
+				*py = *(py + 1);
+				*(py + 1) = tmp;
+			}
+		}
 	}
-	printf("%d", cnt);
+	px = &x;
+	printf("%d", *(px + 1));
 	return 0;
 }
