16aba099fa1ccb7585fe6ba091c6346774817e23
Jeongwon Park <j2982477j@gmail.com>
2023-08-25 13:56:36 +0900

    9장 실습문제 1번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,32 +1,15 @@
 #include <stdio.h>
-int add_digits(int);
 int main() {
-	int n, cur_sum, min, min_con, tmp;
-	scanf("%d", &n);
-	tmp = add_digits(n);
-	while (tmp / 10) 
-		tmp = add_digits(tmp);
-	min = tmp;
-	min_con = n;
-	while (n >= 0) {
-		cur_sum = add_digits(n);
-			while (cur_sum / 10)
-			cur_sum = add_digits(cur_sum);
-		if (cur_sum < min) {
-			min = cur_sum;
-			min_con = n;
-		}
-		scanf("%d", &n);
-	}
-	printf("%d %d", min_con, min);
-	return 0;
-}
+	int x, y, z;
+	int *px, *py, *pz, *tmp;
 
-int add_digits (int n) {
-	int sum = 0;
-	while (n) {
-		sum += n % 10;
-		n /= 10;
-	}
-	return sum;
+	scanf("%d%d%d", &x, &y, &z);
+	px=&x, py=&y, pz=&z;
+	tmp = pz;
+	pz = py;
+	py = px;
+	px = tmp;
+	printf("%d %d %d", *px, *py, *pz);
+
+	return 0;
 }
