ad506b620d95692030cab111caef95b6eb5e70c7
Jeongwon Park <j2982477j@gmail.com>
2023-08-26 21:18:14 +0900

    10장 실습문제 5번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,78 +1,25 @@
 #include <stdio.h>
-void abc(int[],int);
-void cba(int[],int);
 int main() {
-	int n = 0, m = 0, a[10] = { 0 }, b[10] = {0}, tmp, i;
-	int *pa, *pb;
-	for (pa = a;; pa++) {
-		scanf("%d", &tmp);
-		if (tmp == 0)
-			break;
-		*pa = tmp;
-		n++;
-	}
-	for (pb = b;; pb++) {
-		scanf("%d", &tmp);
-		if (tmp == 0)
-			break;
-		*pb = tmp;
-		m++;
-	}
-	abc(a, n);
-	abc(b, m);
-	i = 0;
-	for (pa = a, pb = b; i < n;) {
-		if (*pa < *pb ) {
-			printf("%d ", *pb);
-			pb++;
-		}
-		else {
-			printf("%d ", *pa);
-			pa++;
-		}
+	char a[40], b[20], *pa, *pb;
+	int i = 0, n, a_len = 0, b_len = 0;
+	scanf("%s", a);
+	scanf("%s", b);
+	scanf("%d", &n);
+	while (a[i]) {
+		a_len++;
 		i++;
 	}
-	printf("\n");
-	cba(a, n);
-	cba(b, m);
 	i = 0;
-	for (pa = a, pb = b; i < m;) {
-		if (*pa > *pb) {
-			printf("%d ", *pb);
-			pb++;
-		}
-		else {
-			printf("%d ", *pa);
-			pa++;
-		}
+	while (b[i]) {
+		b_len++;
 		i++;
 	}
-	
+	for (pa = a + a_len + b_len - 1; pa >= a + b_len + n; pa--) {
+		*pa = *(pa - b_len);
+	}
+	for (pa = a + n, pb = b; pb < b + b_len; pa++, pb++)
+		*pa = *pb;
+	a[a_len + b_len] = '\0';
+	printf("%s", a);
 	return 0;
-}
-void abc(int a[],int n) {
-	int *pa,*qa;
-	int tmp;
-	for (pa = a+n-1; pa >=a; pa--) {
-		for (qa = a; qa < pa; qa++) {
-			if (*qa < *(qa + 1)) {
-				tmp = *qa;
-				*qa = *(qa + 1);
-				*(qa + 1) = tmp;
-			}
-		}
-	}
-}
-void cba(int a[], int n) {
-	int *pa, *qa;
-	int tmp;
-	for (pa = a + n - 1; pa >= a; pa--) {
-		for (qa = a; qa < pa; qa++) {
-			if (*qa > *(qa + 1)) {
-				tmp = *qa;
-				*qa = *(qa + 1);
-				*(qa + 1) = tmp;
-			}
-		}
-	}
 }
