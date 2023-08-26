87267e7ad7f3ad9bd02cbb0bfa67d9106c504791
Jeongwon Park <j2982477j@gmail.com>
2023-08-26 15:07:55 +0900

    과제1(포인터) 3번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,44 +1,26 @@
 #include <stdio.h>
-int correct(int *);
-int upper(int *);
 int main() {
-	int lot[6],flg;
-	int *plot;
-	for (plot = lot; plot < lot + 6; plot++) 
-		scanf("%d", plot);
-	flg=correct(lot);
-	printf("%d\n", flg);
-	for (plot = lot; plot < lot + 6; plot++)
-		printf("%d ", *plot);
-	return 0;
-}
-int correct(int *arr) {
-	int *par, *qar,cnt,flg=0;
-	for (par = arr; par < arr + 6; par++) {
-		cnt = 0;
-		for (qar = arr; qar < arr + 6; qar++) {
-			if (*par == *qar)
-				cnt++;
-			if (cnt > 1) {
-				*par = upper(arr);
-				flg = 1;
-				break;
+	int n,sum=0,cur=0;
+	char chr[100],prt_chr[100];
+	char *pchr;
+	scanf("%d", &n);
+	getchar();
+	for (pchr = chr; pchr < chr + n; pchr++)
+		scanf("%c", pchr);
+	for (pchr = chr; pchr < chr + n; pchr++) {
+		if ('0' <= *pchr&&*pchr <= '9') {
+			cur += *pchr - '0';
+			if ('0' <= *(pchr + 1) && *(pchr + 1) <= '9')
+				cur *= 10;
+			else {
+				sum += cur;
+				cur = 0;
 			}
 		}
-	}
-	if (flg == 1)
-		return 1;
-	else
-		return 0;
-}
-int upper(int *arr) {
-	int *par, *qar;
-	int  ret = 1;
-	for (par = arr; par < arr + 6; par++) {
-		for (qar = arr; qar < arr + 6; qar++) {
-			if (ret == *qar) 
-				ret++;
+		else {
+			printf("%c", *pchr);
 		}
 	}
-	return ret;
+	printf("\n%d", sum);
+	return 0;
 }
