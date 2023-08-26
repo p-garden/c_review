2eca17c5f75ac2be74f88a63b4b728324dc7eec4
Jeongwon Park <j2982477j@gmail.com>
2023-08-26 14:18:30 +0900

    과제 1(포인터) 1-2번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,25 +1,58 @@
 #include <stdio.h>
-void abc(int*, int);
+int *max(int[]);
+int *min(int[]);
 int main() {
-	int a[10], i;
-	int *pa;
-	for (pa = a; pa < a + 10; pa++)
-		scanf("%d", pa);
-	for (pa = a,i=10; pa < a + 10; pa++,i--) {
-		abc(pa, i);
+	int n,i,ar[100],tmp,cnt;
+	int *par,*pmax,*pmin;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++) {
+		for (par = ar;; par++) {
+			scanf("%d", par);
+			if (*par == 0)
+				break;
+		}
+		pmax = max(ar), pmin = min(ar);
+		cnt = 0;
+		if (pmax < pmin) {
+			for (par = pmax + 1; par < pmin; par++) {
+				printf("%d ", *par);
+				cnt++;
+			}
+		}
+		else {
+			for (par = pmin + 1; par < pmax; par++) {
+				printf("%d ", *par);
+				cnt++;
+			}
+		}
+		if (cnt == 0)
+			printf("none");
+		printf("\n");
 	}
-	for (pa = a; pa < a + 10; pa++)
-		printf(" %d", *pa);
 	return 0;
 }
-void abc(int*pa, int n) {
-	int max = 0,tmp;
-	int *pb,*pos;
-	for (pb = pa; pb < pa + n; pb++) {
-		if (*pb > *pa) {
-			tmp = *pa;
-			*pa = *pb;
-			*pb = tmp;
+int *max(int a[]) {
+	int max = *a, *pa, *max_pos = a;
+	for (pa = a;; pa++) {
+		if (*pa == 0)
+			break;
+		if (max < *pa) {
+			max = *pa;
+			max_pos = pa;
 		}
 	}
+	return max_pos;
+	
+}
+int *min(int a[]) {
+	int min = *a, *pa, *min_pos = a;
+	for (pa = a;; pa++) {
+		if (*pa == 0)
+			break;
+		if (min > *pa) {
+			min = *pa;
+			min_pos = pa;
+		}
+	}
+	return min_pos;
 }
