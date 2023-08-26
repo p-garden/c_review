e93e36dda8422c0c10fb76c6639d95687ecac797
Jeongwon Park <j2982477j@gmail.com>
2023-08-26 21:18:33 +0900

    10장 실습문제 1번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,25 +1,11 @@
 #include <stdio.h>
 int main() {
-	char a[40], b[20], *pa, *pb;
-	int i = 0, n, a_len = 0, b_len = 0;
+	char a[20];
+	int i;
 	scanf("%s", a);
-	scanf("%s", b);
-	scanf("%d", &n);
-	while (a[i]) {
-		a_len++;
-		i++;
+	for (i = 0; i < 20; i++) {
+		if ('a' <= a[i] && a[i] <= 'z')
+			printf("%c", a[i]);
 	}
-	i = 0;
-	while (b[i]) {
-		b_len++;
-		i++;
-	}
-	for (pa = a + a_len + b_len - 1; pa >= a + b_len + n; pa--) {
-		*pa = *(pa - b_len);
-	}
-	for (pa = a + n, pb = b; pb < b + b_len; pa++, pb++)
-		*pa = *pb;
-	a[a_len + b_len] = '\0';
-	printf("%s", a);
 	return 0;
 }
