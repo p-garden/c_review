1ab2f82caa5d970daa83d0a4ac4173a1afcdf15b
Jeongwon Park <j2982477j@gmail.com>
2023-08-27 11:40:16 +0900

    10장 실습문제 9번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+int check(char*);
 int main() {
-	char a[100], b[50];
+	char a[30];
 	scanf("%s", a);
-	scanf("%s", b);
-	if (strcmp(a, b) > 0) {
-		strcat(a, b);
-		printf("%s", a);
+	printf("%d %d", strlen(a), check(a));
+}
+int check(char *a) {
+	int len,i,j,flg=1;
+	len = strlen(a);
+	for (i = 0, j = len - 1; i < len; i++, j--) {
+		if (a[i] != a[j])
+			flg = 0;
 	}
-	else {
-		strcat(b, a);
-		printf("%s", b);
-	}
+	return flg;
 }
