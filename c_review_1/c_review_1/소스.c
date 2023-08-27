4285a5671f67775818b9fc357d0541fd450b7da0
Jeongwon Park <j2982477j@gmail.com>
2023-08-27 11:07:02 +0900

    10장 실습문제 6번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 int main() {
-	char a[20];
-	int i;
+	char a[40], b[20], *pa, *pb;
+	int i = 0, n, a_len = 0, b_len = 0,m;
 	scanf("%s", a);
-	for (i = 0; i < 20; i++) {
-		if ('a' <= a[i] && a[i] <= 'z')
-			printf("%c", a[i]);
+	scanf("%s", b);
+	scanf("%d", &n);
+	scanf("%d", &m);
+	while (a[i]) {
+		a_len++;
+		i++;
 	}
+	i = 0;
+	while (b[i]) {
+		b_len++;
+		i++;
+	}
+	for (pa = a + a_len + b_len - 1; pa >= a + b_len + n; pa--) {
+		*pa = *(pa - b_len);
+	}
+	if (m == 0) {
+		for (pa = a + n, pb = b; pb < b + b_len; pa++, pb++)
+			*pa = *pb;
+	}
+	else {
+		for (pa = a + n, pb = b+b_len-1; pb >= b ; pa++, pb--)
+			*pa = *pb;
+	}
+	a[a_len + b_len] = '\0';
+	printf("%s", a);
 	return 0;
 }
