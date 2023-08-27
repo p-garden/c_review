ec653d6f88a317195c7615e70875b33d04168396
Jeongwon Park <j2982477j@gmail.com>
2023-08-27 11:34:04 +0900

    10장 실습문제 8번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 int main() {
-	int n,tmp,a=1000,i=0;
-	char *num[] = { "one", "two","three","four","five","six","seven","eight","nine"};
-	char *digit[] = { "THO", "HUN", "TEN","" };
-	scanf("%d", &n);
-	while (a) {
-		tmp = n / a;
-		if (tmp == 0) {
-			a /= 10;
-			i++;
-			continue;
-		}
-		printf("%s %s ", num[tmp-1], digit[i]);
-		i++;
-		n -= tmp * a;
-		a /= 10;
+	char a[100], b[50];
+	scanf("%s", a);
+	scanf("%s", b);
+	if (strcmp(a, b) > 0) {
+		strcat(a, b);
+		printf("%s", a);
 	}
-	return 0;
+	else {
+		strcat(b, a);
+		printf("%s", b);
+	}
 }
