b9039211ef9b29c40ba6563c356069b777b3b8f5
Jeongwon Park <j2982477j@gmail.com>
2023-08-27 15:49:01 +0900

    과제2(문자열) 2-3번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,48 +1,31 @@
 #include <stdio.h>
 #include <string.h>
-void swap(char*, char*,int);
 int main() {
-	int n, i, j,a_b=0,b_a=0;
-	char a[21], b[21],c[201];
+	int n, i, j,b_len=0;
+	char a[100], b[10][100],*pa,tmp[100];
 	gets(a);
-	gets(b);
-	for (i = 1; i <= 10; i++) {
-		swap(a, c, i);
-		if (strcmp(c, b) == 0)
-			a_b = 1;
-	}
-	for (i = 1; i <= 10; i++) {
-		swap(b, c, i);
-		if (strcmp(c, a) == 0)
-			b_a = 2;
-	}
-	printf("%d", a_b+b_a);
-}
-void swap(char*a, char*b, int n) {
-	int len = strlen(a),i,j,tmp,k;
-	for (i = 0,j=0; i < len; i++,j++) {
-		if ('a' <= a[i] && a[i] <= 'z') 
-			b[j] = a[i] - n;
-		else if ('A' <= a[i] && a[i] <= 'Z')
-			b[j] = a[i] + n;
-		else if ('0' <= a[i] && a[i] <= '9') {
-			tmp = a[i] - '0';
-			if ('0' <= a[i + 1] && a[i + 1] <= '9') {
-				tmp *= 10;
-				tmp += a[i + 1] - '0';
-				i++;
-			}
-			for (k = 0; k < n; k++) {
-				b[j] = tmp + 'A' - 1;
-				j++;
-			}
-			j--;
+	for (pa = a,j=0; pa < a + 100; pa++,j++) {
+		if (*pa == '\0') {
+			tmp[j] = '\0';
+			strcpy(b[b_len], tmp);
+			b_len++;
+			break;
 		}
-		else {
-			b[j] = ' ';
+		if (*pa == ' ') {
+			tmp[j] = '\0';
+			strcpy(b[b_len],tmp);
+			j = -1;
+			b_len++;
+			continue;
 		}
-		b[j+1] = '\0';
+		tmp[j] = *pa;
 	}
+	for (i = 0; i < b_len; i++)
+		printf("%s\n", b[i]);
+	strcpy(tmp, b[0]);
+	for (i = 0; i < b_len; i++) {
+		if (strcmp(tmp, b[i]) > 0)
+			strcpy(tmp, b[i]);
+	}
+	printf("%s", tmp);
 }
-
-
