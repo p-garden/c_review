296828285efdc163878b964e5a188f1c419b0fb8
Jeongwon Park <j2982477j@gmail.com>
2023-08-27 11:29:44 +0900

    10장 실습문제 7번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,32 +1,20 @@
 #include <stdio.h>
 int main() {
-	char a[40], b[20], *pa, *pb;
-	int i = 0, n, a_len = 0, b_len = 0,m;
-	scanf("%s", a);
-	scanf("%s", b);
+	int n,tmp,a=1000,i=0;
+	char *num[] = { "one", "two","three","four","five","six","seven","eight","nine"};
+	char *digit[] = { "THO", "HUN", "TEN","" };
 	scanf("%d", &n);
-	scanf("%d", &m);
-	while (a[i]) {
-		a_len++;
+	while (a) {
+		tmp = n / a;
+		if (tmp == 0) {
+			a /= 10;
+			i++;
+			continue;
+		}
+		printf("%s %s ", num[tmp-1], digit[i]);
 		i++;
+		n -= tmp * a;
+		a /= 10;
 	}
-	i = 0;
-	while (b[i]) {
-		b_len++;
-		i++;
-	}
-	for (pa = a + a_len + b_len - 1; pa >= a + b_len + n; pa--) {
-		*pa = *(pa - b_len);
-	}
-	if (m == 0) {
-		for (pa = a + n, pb = b; pb < b + b_len; pa++, pb++)
-			*pa = *pb;
-	}
-	else {
-		for (pa = a + n, pb = b+b_len-1; pb >= b ; pa++, pb--)
-			*pa = *pb;
-	}
-	a[a_len + b_len] = '\0';
-	printf("%s", a);
 	return 0;
 }
