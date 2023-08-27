30ed7667b51ff5509876b73f5fd2dc570709c88e
Jeongwon Park <j2982477j@gmail.com>
2023-08-27 15:24:14 +0900

    과제2(문자열) 1-2번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,23 +1,48 @@
 #include <stdio.h>
 #include <string.h>
+void swap(char*, char*,int);
 int main() {
-	int n, i,cnt,flg=0;
-	char a[100], b[100], *pa,*pb;
+	int n, i, j,a_b=0,b_a=0;
+	char a[21], b[21],c[201];
 	gets(a);
-	scanf("%s", b);
-	for (pa = a; pa < a + strlen(a); pa++) {
-		cnt = 0;
-		if (*pa == b[0]) {
-			for (pb = b, i = 0; pb < b + strlen(b); pb++, i++) {
-				if (*(pa + i) == *pb)
-					cnt++;
-				if (cnt == strlen(b)) {
-					flg += 1;
-					pa += cnt-1;
-					break;
-				}
-			}
-		}
+	gets(b);
+	for (i = 1; i <= 10; i++) {
+		swap(a, c, i);
+		if (strcmp(c, b) == 0)
+			a_b = 1;
 	}
-	printf("%d",flg);
+	for (i = 1; i <= 10; i++) {
+		swap(b, c, i);
+		if (strcmp(c, a) == 0)
+			b_a = 2;
+	}
+	printf("%d", a_b+b_a);
 }
+void swap(char*a, char*b, int n) {
+	int len = strlen(a),i,j,tmp,k;
+	for (i = 0,j=0; i < len; i++,j++) {
+		if ('a' <= a[i] && a[i] <= 'z') 
+			b[j] = a[i] - n;
+		else if ('A' <= a[i] && a[i] <= 'Z')
+			b[j] = a[i] + n;
+		else if ('0' <= a[i] && a[i] <= '9') {
+			tmp = a[i] - '0';
+			if ('0' <= a[i + 1] && a[i + 1] <= '9') {
+				tmp *= 10;
+				tmp += a[i + 1] - '0';
+				i++;
+			}
+			for (k = 0; k < n; k++) {
+				b[j] = tmp + 'A' - 1;
+				j++;
+			}
+			j--;
+		}
+		else {
+			b[j] = ' ';
+		}
+		b[j+1] = '\0';
+	}
+}
+
+
