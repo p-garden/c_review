164899a96abef4c4cde2c1b2d18e3fa63adc6346
Jeongwon Park <j2982477j@gmail.com>
2023-08-29 19:52:46 +0900

    11장 실습문제 6번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,34 +1,71 @@
 #include <stdio.h>
-#include <string.h>
-typedef struct student {
-	char name[8];
-	int kor, eng, math;
-	double avg;
-	char grd;
-}student;
+typedef struct body {
+	int gen, wgh, hgh;
+}body;
+int grd(body);
 int main() {
-	student st[50], *p = st,*q;
-	int n;
-	double avg;
+	body a,*pa;
+	int n, i = 0, cnt[3] = {0},tmp;
 	scanf("%d", &n);
-
-	for (q = p; q < p + n; q++) {
-		scanf("%s", &q->name);
-		scanf("%d%d%d", &q->kor, &q->eng, &q->math);
+	for (i = 0; i < n; i++) {
+		scanf("%d%d%d", &a.gen, &a.wgh, &a.hgh);
+		tmp = grd(a);
+		cnt[tmp - 1] ++;
 	}
-	
-	for (q = p; q < p + n; q++) {
-		printf("%s", q->name);
-		avg = (q->kor + q->eng + q->math) / 3.0;
-		printf(" %.1lf", avg);
-		if (avg >= 90 )
-			printf(" A");
-		else if (avg >= 80)
-			printf(" B");
-		else if (avg >= 70)
-			printf(" C");
-		else
-			printf(" D");
-		printf("\n");
-	}	
+	for (i = 0; i < 3; i++)
+		printf("%d ", cnt[i]);
+}
+int grd (body a) {
+	if (a.gen == 1) {
+		if (a.wgh < 60) {
+			if (a.hgh < 165)
+				return 1;
+			else if (a.hgh < 175)
+				return 2;
+			else
+				return 3;
+		}
+		else if (a.wgh < 70) {
+			if (a.hgh < 165)
+				return 3;
+			else if (a.hgh < 175)
+				return 1;
+			else
+				return 2;
+		}
+		else {
+			if (a.hgh < 165)
+				return 2;
+			else if (a.hgh < 175)
+				return 3;
+			else
+				return 1;
+		}
+	}
+	else {
+		if (a.wgh < 50) {
+			if (a.hgh < 165)
+				return 1;
+			else if (a.hgh < 175)
+				return 2;
+			else
+				return 3;
+		}
+		else if (a.wgh < 60) {
+			if (a.hgh < 165)
+				return 3;
+			else if (a.hgh < 175)
+				return 1;
+			else
+				return 2;
+		}
+		else {
+			if (a.hgh < 165)
+				return 2;
+			else if (a.hgh < 175)
+				return 3;
+			else
+				return 1;
+		}
+	}
 }
