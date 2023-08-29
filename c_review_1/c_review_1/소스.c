6755c32826db9fd52491bf18a668f49fff441a12
Jeongwon Park <j2982477j@gmail.com>
2023-08-29 17:45:08 +0900

    11장 실습문제 1번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,32 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-int in_cnt(char*, char*);
+typedef struct vector {
+	int a, b, c;
+}vector;
 int main() {
-	int cnt;
-	char a[101], b[101];
-	gets(a);
-	scanf("%s", b);
-	cnt = in_cnt(a, b);
-	printf("%d", cnt);
-	
-}
-int in_cnt(char *a, char*b) {
-	int len_a,len_b,i,cnt=0,tmp_cnt;
-	char *pa, *pb;
-	len_a = strlen(a);
-	len_b = strlen(b);
-	for (pa = a; pa < a + len_a; pa++) {
-		if ((*(pa-1) == ' ' || *(pa-1) == '.' || pa == a)&& (*pa==b[0]) && (*(pa+len_b)==' ' || *(pa+len_b)=='.'||*(pa+len_b)=='\0')) {
-			tmp_cnt = 0;
-			for (pb = b,i=0; pb < b + len_b;i++, pb++) {
-				if (*pb == *(pa + i))
-					tmp_cnt++;
-			}
-			if (tmp_cnt == len_b) {
-				cnt++;
-				pa += len_b;
-			}
-		}
-	}
-	return cnt;
+	vector v1, v2;
+	scanf("%d%d%d", &v1.a, &v1.b, &v1.c);
+	scanf("%d%d%d", &v2.a, &v2.b, &v2.c);
+	printf("%d %d %d\n", v1.a*v2.a, v1.b*v2.b, v1.c*v2.c);
+	printf("%d", (v1.a*v2.a + v1.b*v2.b + v1.c*v2.c));
 }
