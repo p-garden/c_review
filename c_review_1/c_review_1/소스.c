3100486697814d544a0369ceb4b5ee47eb3b18ba
Jeongwon Park <j2982477j@gmail.com>
2023-08-29 21:06:52 +0900

    11장 실습문제 12번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,25 +1,47 @@
 #include <stdio.h>
 typedef struct test {
-	int sco;
+	int sco1,sco2,sco3;
 	char name[10];
+	double avg;
 }test;
-test *select_min(test*);
+void read_data(test*);
+void cal_avg(test*);
+void sort(test*);
+void print_score(test*);
 int main() {
-	test a[5], *pa;
-	for (pa = a; pa < a + 5; pa++)
-		scanf("%s %d", &pa->name, &pa->sco);
-	pa = select_min(a);
-	printf("%s %d", pa->name, pa->sco);
+	test a[10], *pa;
+	read_data(a);
+	cal_avg(a);
+	sort(a);
+	print_score(a);
 
 }
-test *select_min(test *a) {
-	test *pa, *pb;
-	int min=101;
-	for (pa = a; pa < a + 5; pa++) {
-		if (min > pa->sco) {
-			min = pa->sco;
-			pb = pa;
+void read_data(test *a) {
+	test *pa;
+	for (pa = a; pa < a + 10; pa++)
+		scanf("%s%d%d%d", &pa->name, &pa->sco1, &pa->sco2, &pa->sco3);
+}
+void cal_avg(test *a) {
+	test *pa;
+	for (pa = a; pa < a + 10; pa++) 
+		pa->avg = (pa->sco1 + pa->sco2 + pa->sco3) / 3.0;
+}
+void sort(test *a) {
+	test *pa, *pb, tmp;
+	for (pa = a+9; pa > a ; pa--) {
+		for (pb = a; pb < pa; pb++) {
+			if (pb->avg < (pb + 1)->avg) {
+				tmp = *pb;
+				*pb = *(pb + 1);
+				*(pb + 1)= tmp;
+			}
 		}
 	}
-	return pb;
+}
+void print_score(test *a) {
+	int i;
+	printf("%s %.2lf\n", a->name, a->avg);
+	printf("%s %.2lf\n", (a + 9)->name, (a + 9)->avg);
+	for (i = 7; i < 10; i++)
+		printf("%s %.2lf\n", (a+i)->name, (a+i)->avg);
 }
