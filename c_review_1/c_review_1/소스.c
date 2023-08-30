bb8c3f23be74aaf4bd4e4f9831a8f2f6f84c7f2c
Jeongwon Park <j2982477j@gmail.com>
2023-08-30 17:41:19 +0900

    12장 실습문제 10번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -2,21 +2,29 @@
 #include <string.h>
 #include <stdlib.h>
 int main() {
-	int n, i, min=101;
-	char **a, tmp[100],ch_min[100];
+	int n, i,j;
+	char **a=NULL, tmp[100];
 	scanf("%d", &n);
 	getchar();
+
 	a = (char**)malloc(n * sizeof(char*));
 	for (i = 0; i < n; i++) {
 		gets(tmp);
 		a[i] = (char*)malloc((strlen(tmp) + 1) * sizeof(char));
 		strcpy(a[i], tmp);
-		if (min > strlen(a[i])) {
-			min = strlen(a[i]);
-			strcpy(ch_min, a[i]);
+	}
+	for (i = n-1; i >0 ; i--) {
+		for (j = 0; j < i; j++) {
+			if(strlen(a[j]) < strlen(a[j + 1])) {
+				strcpy(tmp, a[j]);
+				strcpy(a[j], a[j + 1]);
+				strcpy(a[j + 1], tmp);
+			}
 		}
 	}
-	printf("%s", ch_min);
+	for (i = 0; i < n; i++)
+		printf("%s\n", a[i]);
+
 	for (i = 0; i < n; i++)
 		free(a[i]);
 	free(a);
