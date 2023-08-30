f2bd487814d6c27a35e0703fc0212f3cb42db294
Jeongwon Park <j2982477j@gmail.com>
2023-08-30 16:41:53 +0900

    12장 실습문제 4번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -2,15 +2,37 @@
 #include <string.h>
 #include <stdlib.h>
 int main() {
-	int n, i,d,*p; 
-	scanf("%d", &n);
-	p = (int*)malloc(n * sizeof(int));
-	for (i = 0; i < n; i++)
-		scanf("%d", &p[i]);
-	scanf("%d", &d);
-	p = (int*)realloc(p, (n-d)*sizeof(int));
-	for (i = 0; i < n - d; i++)
-		printf("%d\n", p[i]);
+	int n, m,i,j,bs;
+	char **p, alp;
+	scanf("%d%d", &n, &m);
+	p = (int**)malloc(m * sizeof(int*));
+	for (i = 0; i < m; i++)
+		p[i] = (int*)malloc(n * sizeof(int));
+	
+	alp = 'a';
+	bs = 0;
+	for (i = 0; i < m; i++) {
+		for (j = 0; j < n; j++) {
+			p[i][j] = alp;
+			alp++;
+			if (alp > 'z'&& bs == 0) {
+				alp = 'A';
+				bs = 1;
+			}
+			else if (alp > 'Z' && bs == 1) {
+				alp = 'a';
+				bs = 0;
+			}
+		}
+	}
+	for (i = 0; i < m; i++) {
+		for (j = 0; j < n; j++)
+			printf("%c ", p[i][j]);
+		printf("\n");
+	}
+		
+	for (i = 0; i < m; i++)
+		free(p[i]);
 	free(p);
 	return 0;
 }
