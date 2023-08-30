0e24d1361b398c8ab1900f2141414e28af82f01b
Jeongwon Park <j2982477j@gmail.com>
2023-08-30 16:49:53 +0900

    12장 실습문제 5번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -2,37 +2,26 @@
 #include <string.h>
 #include <stdlib.h>
 int main() {
-	int n, m,i,j,bs;
-	char **p, alp;
-	scanf("%d%d", &n, &m);
-	p = (int**)malloc(m * sizeof(int*));
-	for (i = 0; i < m; i++)
-		p[i] = (int*)malloc(n * sizeof(int));
+	int n,cnt1=0, cnt2=0,i;
+	char *a,ch1,ch2;
+	scanf("%d", &n);
+	getchar();
+	a = (char*)malloc((n+1) * sizeof(char));
+	scanf("%s", a);
 	
-	alp = 'a';
-	bs = 0;
-	for (i = 0; i < m; i++) {
-		for (j = 0; j < n; j++) {
-			p[i][j] = alp;
-			alp++;
-			if (alp > 'z'&& bs == 0) {
-				alp = 'A';
-				bs = 1;
-			}
-			else if (alp > 'Z' && bs == 1) {
-				alp = 'a';
-				bs = 0;
-			}
-		}
+	getchar();
+	scanf("%c", &ch1);
+	getchar();
+	scanf("%c", &ch2);
+
+	for (i = 0; i < n; i++) {
+		if (ch1 == a[i])
+			cnt1++;
+		if (ch2 == a[i])
+			cnt2++;
 	}
-	for (i = 0; i < m; i++) {
-		for (j = 0; j < n; j++)
-			printf("%c ", p[i][j]);
-		printf("\n");
-	}
-		
-	for (i = 0; i < m; i++)
-		free(p[i]);
-	free(p);
+	printf("%d %d", cnt1, cnt2);
+
+	free(a);
 	return 0;
 }
