0fc1f06e9d3be2f25b7fa41f3e99db3c5e30c687
Jeongwon Park <j2982477j@gmail.com>
2023-08-30 17:05:14 +0900

    12장 실습문제 7번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -1,28 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-typedef struct student {
-	char name[8];
-	int kor, eng, math;
-	float avg;
-
-}student;
 int main() {
-	int n,i;
-	student *a;
+	int n, i,*a,tmp;
 	scanf("%d", &n);
-	a = (student*)malloc(n * sizeof(student));
-	for (i = 0; i < n; i++) {
-		scanf("%s%d%d%d", a[i].name, &a[i].kor, &a[i].eng, &a[i].math);
-		a[i].avg = (a[i].kor + a[i].eng + a[i].math) / 3.0;
+	a = (int*)malloc(n * sizeof(int));
+	for (i = 0; i < n; i++)
+		scanf("%d", &a[i]);
+
+	for (i = 0; i < n-1; i++) {
+		if (a[i] > a[i + 1]) {
+			tmp = a[i];
+			a[i] = a[i + 1];
+			a[i + 1] = tmp;
+		}
 	}
 	for (i = 0; i < n; i++) {
-		printf("%s %.1f ", a[i].name,a[i].avg);
-		if (a[i].kor >= 90 || a[i].eng >= 90 || a[i].math >= 90)
-			printf("GREAT ");
-		if (a[i].kor < 70 || a[i].eng < 70 || a[i].math < 70)
-			printf("BAD ");
-		printf("\n");
+		printf("%d\n", a[i]);
 	}
 	free(a);
 	return 0;
