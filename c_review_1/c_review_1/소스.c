41b0fb3b1663e9e393577ab9ac90124b0f8ca6de
Jeongwon Park <j2982477j@gmail.com>
2023-08-30 19:20:11 +0900

    12장 실습문제 12번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -2,25 +2,20 @@
 #include <string.h>
 #include <stdlib.h>
 int main() {
-	int i,j, *a,add=5,cnt=0,tmp[20];
-	a = (int*)malloc(add * sizeof(int));
-	while (1) {
-		scanf("%d", &a[cnt]);
-		if (a[cnt] == -1)
-			break;
-		cnt++;
-		if (cnt == add) {
-			add += 3;
-			for (j = 0; j < cnt; j++) 
-				tmp[j] = a[j];
-			free(a);
-			a = (int*)malloc(add * sizeof(int));
-			for (j = 0; j < cnt; j++) 
-				a[j] = tmp[j];
-		}
+	int n,*a,*b,i;
+	scanf("%d", &n);
+	a = (int*)malloc(n * sizeof(int));
+	for (i = 0; i < n; i++)
+		a[i] = i;
+	b = (int*)malloc((n - 1) * sizeof(int));
+	for (i = 0; i < n-1; i++) {
+		a[i] = i;
+		if (i >= n / 2)
+			a[i]++;
 	}
-	for (i = 0; i < cnt+1; i++)
+	for (i = 0; i < n - 1; i++)
 		printf(" %d", a[i]);
 	free(a);
+	free(b);
 	return 0;
 }
