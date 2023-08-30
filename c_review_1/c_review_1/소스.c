eee1f8b83734c5ea00300d449e242d68ba04b70e
Jeongwon Park <j2982477j@gmail.com>
2023-08-30 16:23:38 +0900

    12장 실습문제 2번

--- c_review_1/c_review_1/소스.c
+++ c_review_1/c_review_1/소스.c
@@ -2,15 +2,19 @@
 #include <string.h>
 #include <stdlib.h>
 int main() {
-	int n, *p,i,sum=0;
+	float  *p, max;
+	int n, i; 
 	scanf("%d", &n);
 
-	p = (int*)malloc(n * sizeof(int));
-	for (i = 0; i < n; i++) {
-		scanf("%d", &p[i]);
-		sum += p[i];
+	p = (float  *)malloc(n * sizeof(float));
+	scanf("%f", &p[0]);
+	max = p[0];
+	for (i = 1; i < n; i++) {
+		scanf("%f", &p[i]);
+		if (max < p[i])
+			max = p[i];
 	}
-	printf("%d", sum);
+	printf("%.2f", max);
 	free(p);
 	return 0;
 }
